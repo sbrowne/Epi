5751d9f673ed46360eaa917f99b99d8397f5dfe4
sbrowne <sbrowne20uk@yahoo.co.uk>
2016-10-18 16:07:32 +0100

    Add project files.

---
+++ Episerver9/Models/Pages/StartPage.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Episerver9.Models.Pages
+{
+    [ContentType]
+    public class StartPage : PageData
+    {
+        public virtual string Heading { get; set; } 
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
