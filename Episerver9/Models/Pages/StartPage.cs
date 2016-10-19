dcd34729ef64e24fc93c380935ce1646e281d82d
sbrowne <sbrowne20uk@yahoo.co.uk>
2016-10-19 09:59:14 +0100

    test

--- Episerver9/Models/Pages/StartPage.cs
+++ Episerver9/Models/Pages/StartPage.cs
@@ -10,7 +10,7 @@ namespace Episerver9.Models.Pages
     [ContentType]
     public class StartPage : PageData
     { 
-        public virtual string Heading { get; set; } 
+        public virtual string Heading { get; set; }  // test
         public virtual XhtmlString MainBody { get; set; }   
     }
 }
